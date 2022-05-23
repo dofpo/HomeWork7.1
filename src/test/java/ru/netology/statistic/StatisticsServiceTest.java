e7d221e193de9e874458146c7c725cf5abb1f8d2
dofpo <z-salawat91@mail.ru>
2022-05-23 15:21:38 +0500

    creat StatisticsServiceTest

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -30,4 +30,16 @@ public class StatisticsServiceTest {
         assertEquals(expected, actual);
 
     }
+    @Test
+    void findMin1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 13};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+
+    }
 }
